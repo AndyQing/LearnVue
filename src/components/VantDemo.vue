0865ce6a1650daaee92af27f1f2b00193ec40df9
伪淑女 <5642012+fake_lady@user.noreply.gitee.com>
2020-07-13 17:38:06 +0800

    vant练习

---
+++ src/components/VantDemo.vue
@@ -0,0 +1,53 @@
+<template>
+  <van-tabs v-model="active">
+    <van-tab title="标签 1">
+      <div class="content">
+        内容 1
+        <br />内容 2
+        <br />内容 3
+        <br />内容 4
+        <br />内容 5
+        <br />内容 6
+        <br />内容 7
+        <br />内容 8
+      </div>
+    </van-tab>
+    <van-tab title="标签 2">
+      <div class="content">内容 2</div>
+    </van-tab>
+    <van-tab title="标签 3">内容 3</van-tab>
+    <van-tab title="标签 4">内容 4</van-tab>
+  </van-tabs>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+.content {
+  /* height: 100%; */
+  background: red;
+  line-height: 100px;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+.van-tabs {
+  height: 100%;
+}
+.van-tabs__content {
+  height: calc(100% - 44px);
+  overflow-y: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 // import App from './APP'
-import App from './components/FatherCom'
+import App from './components/VantDemo'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
-// Vue.use(VueRouter);
+Vue.use(Vant);
+
 Vue.use(ViewUI);
 
 Vue.use(ElementUI)
